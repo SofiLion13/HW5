290c7be305c0c5802c5a03b92640a609195376d0
Sofia <dragsy13@bk.ru>
2023-02-26 15:57:39 +0300

    Add new task

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,11 +1,9 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] number = new int[size];
+int[] number = new int[8];
 Random rnd = new Random();
-for (int i = 0; i < size; i++)
+for (int i = 0; i < 8; i++)
 {
     number[i] = rnd.Next(0, 100);
 }
